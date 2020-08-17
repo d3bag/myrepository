347fa84cefd46baceab502daed246a9fee20df26
d3bag <darknet.bit@bk.ru>
2020-08-17 18:19:50 +0300

    Добавьте файлы проекта.

---
+++ LinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace StudyCons
+{
+    public class LinkedList<T>
+    {
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+
+            if (head == null)
+                head = node;
+            else
+            {
+                tail.Next = node;
+                node.Previous = tail;
+            }
+                
+            tail = node;
+            count++;
+        }
+        public bool Remove(T data)
+        {
+            Node<T> current = head; // Объект удаления
+            Node<T> previous = null; // Объект перед объектом удаления
+
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head.Next;
+
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }        
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudyCons
+{
+    class Program
+    {
+        static LinkedList<string> linkedList;
+        static void Main(string[] args)
+        {
+            linkedList = new LinkedList<string>();
+
+            linkedList.Add("Vlad");
+            linkedList.Add("Sima");
+            Output();
+
+            linkedList.Remove("Sima");
+            Output();
+        }
+        static void Output()
+        {
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
